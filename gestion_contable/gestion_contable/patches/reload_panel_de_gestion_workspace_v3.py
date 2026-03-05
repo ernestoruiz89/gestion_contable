c9512c2b2df5391569febf94eba3fe40c7fe1387
Ernesto Ruiz <eruiz@AsoFinde.local>
2026-03-04 20:13:47 -0600

    chore(patches): bump workspace reload patch to v3

---
+++ gestion_contable/gestion_contable/patches/reload_panel_de_gestion_workspace_v3.py
@@ -0,0 +1,8 @@
+﻿import frappe
+
+
+def execute():
+    """Force reload of Panel de Gestion workspace from JSON during migrate (v3)."""
+    frappe.reload_doc("gestion_contable", "workspace", "panel_de_gestion", force=True)
+
+
