968a5d7244ec1d41cac28bcb550cbea834212e67
Ernesto Ruiz Escorcia <eruiz@wbapps.com>
2026-03-07 23:10:22 -0600

    fix: add missing .py modules for new DocTypes

---
+++ gestion_contable/gestion_contable/doctype/cifra_nota_estado_financiero/cifra_nota_estado_financiero.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Despacho and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class CifraNotaEstadoFinanciero(Document):
+	pass
