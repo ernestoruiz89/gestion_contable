d70584544ea437d6e38c9ac670008335645d0358
Ernesto Ruiz Escorcia <eruiz@wbapps.com>
2026-03-08 00:41:57 -0600

    fix: align eeff version controller naming

--- gestion_contable/gestion_contable/doctype/version_documento_eeff/version_documento_eeff.py
+++ gestion_contable/gestion_contable/doctype/version_documento_eeff/version_documento_eeff.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2026, Despacho and contributors
 # For license information, please see license.txt
 
-import frappe
 from frappe.model.document import Document
 
 
-class VersionDocumentoEeff(Document):
-	pass
+class VersionDocumentoEEFF(Document):
+    pass
+
+
+# Backward-compatible alias in case Frappe resolves the acronym as title-case.
+class VersionDocumentoEeff(VersionDocumentoEEFF):
+    pass
